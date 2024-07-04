68fa735dca569e3a273fe09955c676e8f46202b1
shaaaaz <jiwanishaaz@gmail.com>
2024-07-04 19:40:49 +0530

    users-2

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const express = require("express")
-const { connectDB , disconnectDB , isConnected} = require('./db.js')
+const { connectDB , disconnecYtDB , isConnected} = require('./db.js')
 const app = express()
 const Router = require('./routes.js')
 const PORT = 3000
 const cors = require('cors')
 
+app.use(cors())
+
 app.options('*', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "https://studio-frontend-ampy6loh9-shaazs-projects-888212a7.vercel.app");
+    res.header("Access-Control-Allow-Origin",'https://studio-frontend-phi.vercel.app','https://studio-frontend-phi.vercel.app/signup','https://studio-frontend-phi.vercel.app/login','http://localhost:5173/login','http://localhost:5173/signup', "https://studio-frontend-ampy6loh9-shaazs-projects-888212a7.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.sendStatus(200)
   })     
 
-app.use(cors())
 
-app.use(cors({
-    origin: ['https://studioo-h3vda8igl-shaazs-projects-888212a7.vercel.app/','http://localhost:5173'],
-    methods: 'GET, POST, PUT, DELETE',
-    allowedHeaders: 'Content-Type',
-    credentials: true,
-}))
+// app.use(cors({
+//     origin: ['https://studio-frontend-phi.vercel.app','https://studio-frontend-phi.vercel.app/signup','https://studio-frontend-phi.vercel.app/login','http://localhost:5173/login','http://localhost:5173/signup','https://studioo-h3vda8igl-shaazs-projects-888212a7.vercel.app/','http://localhost:5173'],
+//     methods: 'GET, POST, PUT, DELETE',
+//     allowedHeaders: 'Content-Type',
+//     credentials: true,
+// }))
 
 const bodyParser = require('body-parser')
 
--- routes.js
+++ routes.js
@@ -25,7 +25,7 @@ router.post('/login', async (req, res) => {
         const { username, password } = req.body
         const user = await userModel.findOne({ username, password })
         if (!user) {
-            return res.status(401).json({ error: 'Invalid username or password' });
+            return res.status(201).json({ error: 'Invalid username or password' });
         } else {
             return res.status(200).json(user);
         }
@@ -1073,6 +1073,21 @@ router.delete('/deleteList/:id/:category/:listid', async (req, res) => {
         return res.json(user)
     }
     catch (err) {
+        console.log(err)
+    }
+})
+
+router.get('/users2', async(req,res) => {
+    console.log("here")
+    try{
+        const usersList = await userModel.find({}, '_id name username profilePic')
+        if(!usersList){
+            res.send("No Users Found")
+            return
+        }
+        res.send(usersList)
+    }
+    catch(err){
         console.log(err)
     }
 })
